85b21ce997b52cef787e78b5f9ea9c046af6fd27
Motoxpro <35399604+Motoxpro@users.noreply.github.com>
2024-05-18 10:41:27 +0200

    Update analysis.ts

--- app/routes/results/analysis/analysis.ts
+++ app/routes/results/analysis/analysis.ts
@@ -24,7 +24,7 @@ export const analyzeRiders = (data: RaceData[]): (Set<string> | RiderAnalysis[])
       return;
     }
     race.SplitTimes.forEach((split) => {
-      const riderId = split.Riders.UciRiderId + split.Run;
+      const riderId = `${split.Riders.UciRiderId} ${split.Run}`;
       if (!riderMap[riderId]) {
         riderMap[riderId] = {
           FullName: split.Riders.FullName,
